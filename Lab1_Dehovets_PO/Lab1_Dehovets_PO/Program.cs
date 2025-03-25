ef7626abcc31f2e29800513d8e842eefba2d8c68
NadiDg <nadia219697@gmail.com>
2025-03-26 00:07:41 +0200

    method GenerateMatrix
    Generate a random 2D matrix

--- Lab1_Dehovets_PO/Lab1_Dehovets_PO/Program.cs
+++ Lab1_Dehovets_PO/Lab1_Dehovets_PO/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static Random globalRand = new();
+    static object randLock = new();
+
+    static int[,] GenerateMatrix(int size)
+    {
+        int[,] matrix = new int[size, size];
+
+        lock (randLock)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = globalRand.Next(-1000, 1000);
+                }
+            }
+        }
+        return matrix;
+    }
+    
+
+    static void Main()
+    {
+        
+
+    }
+}
